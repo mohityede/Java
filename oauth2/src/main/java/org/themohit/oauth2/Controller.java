090acd9ae8b2bd1e0928b9565fe446e9aa8e2e06
mohityede <mohityede755@gmail.com>
2024-07-11 20:36:33 +0530

    oauth2 practice

---
+++ oauth2/src/main/java/org/themohit/oauth2/Controller.java
@@ -0,0 +1,12 @@
+package org.themohit.oauth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String home(){
+        return "Welcome home";
+    }
+}
